1d0cc40f81601ee2dd11cdb22f204e58f0f363df
amir <amir@LAPTOP-GAPH6BPE>
2022-11-04 11:45:34 +0200

    changes

--- 21 Game/Deck.cs
+++ 21 Game/Deck.cs
@@ -11,27 +11,44 @@ namespace _21_Game
         public List<Card> cards;
         public Deck() 
         {
+            
+            //picture cards adding:
+            for (int i = 0; i < 12; i++)
             {
-                for (int i = 0;i < 9;i++)
-                {
-                    Card card = new Card(Card.Shape.HEART,i);
-                    cards.Add(card);
-                }
-                for (int i = 0; i < 9; i++)
-                {
-                    Card card = new Card(Card.Shape.SPADE, i);
-                    cards.Add(card);
-                }
-                for (int i = 0; i < 9; i++)
-                {
-                    Card card = new Card(Card.Shape.DIAMOND, i);
-                    cards.Add(card);
-                }
-                for (int i = 0; i < 9; i++)
-                {
-                    Card card = new Card(Card.Shape.CLUB, i);
-                    cards.Add(card);
-                }
+                Card card = new Card(Card.Shape.CLUB, 10);
+                cards.Add(card);
+            }
+        }
+        private  void CreateNumCards()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Card card = new Card(Card.Shape.HEART, i);
+                cards.Add(card);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                Card card = new Card(Card.Shape.SPADE, i);
+                cards.Add(card);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                Card card = new Card(Card.Shape.DIAMOND, i);
+                cards.Add(card);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                Card card = new Card(Card.Shape.CLUB, i);
+                cards.Add(card);
+            }
+        }
+        private void CreatePictureCards()
+        {
+            //picture cards adding:
+            for (int i = 0; i < 12; i++)
+            {
+                Card card = new Card(Card.Shape.CLUB, 10);
+                cards.Add(card);
             }
         }
 
